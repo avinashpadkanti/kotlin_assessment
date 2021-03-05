e04f85c5654754e51cf699d39f8a863a46a47bb5
avinashpadkanti <80074926+avinashpadkanti@users.noreply.github.com>
2021-03-05 10:16:54 +0530

    Step-2 Code
    Updated code according to step-2 requirement

--- Assessment/src/main/kotlin/main.kt
+++ Assessment/src/main/kotlin/main.kt
@@ -1,13 +1,32 @@
 fun main(args: Array<String>) {
 
-    println("Total price is: $"+calculateTotal(args))
+    println("Total price is: $"+"%.2f".format(calculateTotal(args)))
 }
 
 fun calculateTotal(args: Array<String>): Double {
-    val pricesMap = mutableMapOf("apple" to 0.60, "orange" to 0.25)
-    var total : Double  = 0.0
-    for (productName in args)
-        total += pricesMap.get("${productName.toLowerCase().trim()}")!!
+    return calculateTotalWithOffers(calculateCartCount(args))
+}
 
-    return total
+fun calculateCartCount(args: Array<String>): MutableMap<String, Int> {
+    val finalCart = mutableMapOf("apple" to 0, "orange" to 0)
+    for (productName in args) {
+        var tempProductName = productName.toLowerCase().trim()
+        finalCart.get(tempProductName)?.plus(1)?.let { finalCart.put(tempProductName, it) }
+    }
+    return finalCart
+}
+
+fun calculateTotalWithOffers(cartItems: MutableMap<String, Int>) : Double{
+
+    var applePrice = 0.60;
+    var orangePrice = 0.25;
+
+    var appleCount = cartItems.get("apple")
+    var orangeCount = cartItems.get("orange")
+
+    var appleCost : Double = appleCount?.times(.5)?.times(applePrice) ?: 0.0
+    var orangeCost : Double = orangeCount?.times(0.6666666666666667)?.times(orangePrice) ?: 0.0
+
+    return appleCost+orangeCost;
+
 }
--- Assessment/src/test/kotlin/MainKtTest.kt
+++ Assessment/src/test/kotlin/MainKtTest.kt
@@ -4,16 +4,26 @@ import org.junit.jupiter.api.Test
 internal class MainKtTest{
     @Test
     fun testMainHappyPath(){
-        assertEquals(2.05, calculateTotal(arrayOf("apple", "apple", "orange", "apple")))
+        assertEquals(1.0666666666666667, calculateTotal(arrayOf("apple", "apple", "orange", "apple")))
     }
 
     @Test
     fun testMainCaps(){
-        assertEquals(2.05, calculateTotal(arrayOf("APPLE", "APPLE", "ORANGE", "APPLE")))
+        assertEquals(1.0666666666666667, calculateTotal(arrayOf("APPLE", "APPLE", "ORANGE", "APPLE")))
     }
 
     @Test
     fun testMainWhiteSpaces(){
-        assertEquals(2.05, calculateTotal(arrayOf("  APPLE   ", "APPLE   ", "ORANGE   ", "APPLE    ")))
+        assertEquals(1.0666666666666667, calculateTotal(arrayOf("  APPLE   ", "APPLE   ", "ORANGE   ", "APPLE    ")))
+    }
+
+    @Test
+    fun testMain_4_Apples(){
+        assertEquals(1.3666666666666667, calculateTotal(arrayOf("apple", "apple", "orange", "apple", "apple")))
+    }
+
+    @Test
+    fun testMain_6_Oranges(){
+        assertEquals(2.2, calculateTotal(arrayOf("apple", "apple", "orange", "apple", "apple", "orange", "orange", "orange", "orange", "orange")))
     }
 }
